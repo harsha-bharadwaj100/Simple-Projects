94b9fdc5e1296ad5f4470eb2ab16f3ee7579b33c
Harsha Bharadwaj <88173012+harsha-bharadwaj100@users.noreply.github.com>
2024-11-01 14:46:15 +0530

    LRU

--- Page Replacement Algorithms.py
+++ Page Replacement Algorithms.py
@@ -20,6 +20,29 @@ def FIFO(seq, n):
     return pagefaults, hits
 
 
+def LRU(seq, n):
+    pagefaults = []
+    hits = []
+    frame = [None] * n
+    pos = 0
+    lru = dict()
+    for i in range(len(seq)):
+        lru[seq[i]] = i
+        if seq[i] not in frame:
+            if None in frame:
+                frame[pos] = seq[i]
+                pos = pos + 1
+            else:
+                pos = frame.index(min(frame, key=lambda x: lru[x]))
+                frame[pos] = seq[i]
+            pagefaults.append(i)
+            print(frame, lru, seq[i], "pagefault")
+        else:
+            hits.append(i)
+            print(frame, lru, seq[i], "hit")
+    return pagefaults, hits
+
+
 print(FIFO(sequence, fsize))
-# print(LRU(sequence, fsize))
+print(LRU(sequence, fsize))
 # print(OPT(sequence, fsize))
